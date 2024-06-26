b830c47f12a175ff8dec638959352fc82ac7c070
richarquispe <richarquispe@users.noreply.github.com>
2024-06-26 17:25:08 +0000

    deploy: 625c77e39a74bb898d4ced85c8349d7a9fc425d5

---
+++ precache-manifest.6e4e8d56ade39237b054466fa695eb82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafe12175d963d5f474808ad28c049ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36362d1fd5aa5dfaae8d",
+    "url": "/static/css/2.8e2610c1.chunk.css"
+  },
+  {
+    "revision": "147fe53eb93019cbb850",
+    "url": "/static/css/main.6ce979a3.chunk.css"
+  },
+  {
+    "revision": "36362d1fd5aa5dfaae8d",
+    "url": "/static/js/2.a3db1ed1.chunk.js"
+  },
+  {
+    "revision": "cab22c2f853eb971190951a87268c280",
+    "url": "/static/js/2.a3db1ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "147fe53eb93019cbb850",
+    "url": "/static/js/main.2bf983c6.chunk.js"
+  },
+  {
+    "revision": "79e7a9dc5138f5596451",
+    "url": "/static/js/runtime-main.660e20f8.js"
+  }
+]);
